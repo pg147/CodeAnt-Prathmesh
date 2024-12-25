f39ef3d5b33cbe37c66811de4612a969588f623d
pg147 <xyzprathamesh147@gmail.com>
2024-12-26 01:23:49 +0530

    Custom Colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,17 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#1570EF',
+        light: '#FAFAFA',
+        outline: '#E9EAEB',
+        subHeading: "#6A6A6A"
+      },
       fontFamily: {
         outfit: ['Outfit', 'sans-serif']
+      },
+      boxShadow: {
+        intense: '0px 0px 16px rgba(17,17,26,0.1)'
       }
     },
   },
