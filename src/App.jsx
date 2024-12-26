9d317ec04a62105b1ab302ab58e3c0f21f225a35
pg147 <xyzprathamesh147@gmail.com>
2024-12-26 22:30:49 +0530

    Route added for Signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+// React Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
 import SignIn from "./pages/sign-in/SignIn";
 
 export default function App() {
   return (
-    <div className="">
-      <SignIn />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   )
 }
