357dcdea4bd11507b2f39328dd751ba4a25f1fdb
pg147 <xyzprathamesh147@gmail.com>
2024-12-26 22:44:33 +0530

    Route added for Repository

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import SignIn from "./pages/sign-in/SignIn";
+import Repository from "./pages/repository/Repository";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/repository" element={<Repository />} />
       </Routes>
     </Router>
   )
